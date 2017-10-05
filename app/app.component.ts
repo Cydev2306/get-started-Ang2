f56b79adb5ba795950b476c36e9087f05a4121fb
charlier cyril <charlier.cy@gmail.com>
2017-10-05 08:06:28 +0200

    home module + hike service

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,7 +14,14 @@ export class AppComponent  {
 	constructor(private _hikeService: HikeService) {
 	}
 	ngOnInit(){
-		this.hikes = this._hikeService.getHikes();
+		// this.hikes = this._hikeService.getHikes();
+		// the old one
+		this._hikeService.getHikesFromAPI()
+			.subscribe(
+				res => this.hikes = res,
+				err => console.error(err.status)
+			)
+			// 3 cb en param ok, err, done 
 		console.log(this.hikes);
 	}
  }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HikeModule } from './hike/hike.module';
+import { HomeModule } from './home/home.module';
+
 @NgModule({
-  imports:      [ BrowserModule, HikeModule ],
+  imports:      [ BrowserModule, HikeModule, HttpModule, HomeModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
